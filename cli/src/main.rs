b3f81f4263860ffad6776318c1ef9c4d8d676314
l3r8yJ <l3r8y@duck.com>
2024-06-22 19:43:51 +0300

    feat(#13): separate tests module

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -28,21 +28,3 @@ mod args;
 fn main() {
     let _ = Args::parse();
 }
-
-#[cfg(test)]
-mod tests {
-    use std::str;
-
-    use anyhow::Result;
-    use assert_cmd::Command;
-
-    #[test]
-    fn should_output_help() -> Result<()> {
-        let assertion = Command::cargo_bin("cli")?.arg("--help").assert();
-        let bytes = assertion.get_output().stdout.as_slice();
-        let output = str::from_utf8(bytes)?;
-        assert!(output.contains("The port to run"));
-        assert!(output.contains("3000"));
-        Ok(())
-    }
-}
