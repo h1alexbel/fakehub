6f52d45b0dd1feb74c58b4dc80480d91b4365470
h1alexbel <h1alexbelx@gmail.com>
2024-09-02 10:20:42 +0300

    feat(#124): one github

--- server/src/objects/fakehub.rs
+++ server/src/objects/fakehub.rs
@@ -37,154 +37,61 @@ use uuid::Uuid;
 /// let jeff = github.user("jeff").expect("Failed to get user");
 /// assert_that!(&jeff.username, is(equal_to("jeff")));
 /// ```
-/// To add new GitHub:
-/// ```
-/// use hamcrest::assert_that;
-/// use hamcrest::{equal_to, is, HamcrestMatcher};
-/// use std::collections::HashMap;
-/// use uuid::Uuid;
-/// use server::objects::fakehub::Fakehub;
-/// use server::objects::github::GitHub;
-///
-/// let mut fakehub = Fakehub::default();
-/// let custom = String::from("google");
-/// let expected = Uuid::new_v4();
-/// fakehub.add(GitHub {id: expected, name: custom.clone(), users: HashMap::new(),});
-/// let created = fakehub.browser.get(&custom).expect("Failed to get GitHub");
-/// assert_that!(created.id, is(equal_to(expected)));
-/// assert_that!(created.clone().name, is(equal_to(custom)));
-/// ```
 #[derive(Clone)]
 pub struct Fakehub {
-    /// GitHubs.
-    pub hubs: HashMap<Uuid, GitHub>,
-    /// Prefixed GitHubs.
-    pub browser: HashMap<String, GitHub>,
+    /// GitHub.
+    pub github: GitHub
 }
 
 impl Default for Fakehub {
     fn default() -> Fakehub {
-        let mut hubs: HashMap<Uuid, GitHub> = HashMap::new();
-        let id = Uuid::new_v4();
         let mut users: HashMap<String, User> = HashMap::new();
         let jeff = String::from("jeff");
         users.insert(jeff.clone(), User::new(jeff));
-        hubs.insert(
-            id,
-            GitHub {
-                id,
-                name: String::from("main"),
-                users,
-            },
-        );
         Fakehub {
-            hubs: hubs.clone(),
-            browser: hubs.into_values().map(|v| (v.clone().name, v)).collect(),
+            github: GitHub {
+                id: Uuid::new_v4(),
+                name: String::from("main"),
+                users
+            }
         }
     }
 }
 
+// @todo #124:90min Think about GitHub Enterprise GitHubs feature.
+//  Let's think about how we can simulate multiple GitHub instances like GitHub
+//  platform does with GitHub Enterprise feature. Fakehub should have multiple
+//  GitHubs inside, and user can pick to which GitHub he wants store the
+//  testing data.
 impl Fakehub {
-    /// Add new GitHub
-    pub fn add(&mut self, github: GitHub) {
-        self.hubs.insert(github.id, github.clone());
-        self.browser.insert(github.clone().name, github);
-    }
-
-    /// All GitHub instances.
-    pub fn githubs(self) -> Vec<GitHub> {
-        self.hubs
-            .iter()
-            .map(|(k, v)| GitHub {
-                id: *k,
-                name: v.name.clone(),
-                users: v.users.clone(),
-            })
-            .collect()
-    }
-
     /// Main GitHub.
-    pub fn main(&self) -> &GitHub {
-        self.browser.get("main").expect("Failed to get main GitHub")
+    pub fn main(self) -> GitHub {
+        self.github
     }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::objects::fakehub::Fakehub;
-    use crate::objects::github::GitHub;
     use anyhow::Result;
     use hamcrest::{equal_to, is, HamcrestMatcher};
-    use std::collections::HashMap;
-    use uuid::Uuid;
 
     #[test]
-    fn creates_default_fakehub() -> Result<()> {
+    fn returns_default_fakehub_instance() -> Result<()> {
         let fakehub = Fakehub::default();
-        let default = fakehub.browser.get("main").expect("Failed to get GitHub");
+        let default = fakehub.main();
         assert_that!(default.id.is_nil(), is(equal_to(false)));
         Ok(())
     }
 
     #[test]
-    fn adds_github() -> Result<()> {
-        let mut fakehub = Fakehub::default();
-        let url = String::from("google");
-        let expected = Uuid::new_v4();
-        fakehub.add(GitHub {
-            id: expected,
-            name: url.clone(),
-            users: HashMap::new(),
-        });
-        let github = fakehub.browser.get(&url).expect("Failed to get GitHub");
-        assert_that!(github.id, is(equal_to(expected)));
-        Ok(())
-    }
-
-    #[test]
-    fn returns_all_github_instances_after_add() -> Result<()> {
-        let mut fakehub = Fakehub::default();
-        let custom = String::from("myStartup");
-        fakehub.add(GitHub {
-            id: Uuid::new_v4(),
-            name: custom.clone(),
-            users: HashMap::new(),
-        });
-        let instances = fakehub.githubs();
-        assert_that!(instances.len(), is(equal_to(2)));
-        Ok(())
-    }
-
-    #[test]
-    fn returns_default_instance() -> Result<()> {
-        let fakehub = Fakehub::default();
-        let instances = fakehub.githubs();
-        let github = instances.first().expect("Failed to get GitHub");
-        let users = github.clone().users();
-        let user = users.first().expect("Failed to get user");
-        assert_that!(&github.name, is(equal_to("main")));
-        assert_that!(&user.username, is(equal_to("jeff")));
-        Ok(())
-    }
-
-    #[test]
-    fn returns_github_with_users() -> Result<()> {
-        let fakehub = Fakehub::default();
-        let github = fakehub.browser.get("main").expect("Failed to get GitHub");
-        let users = github.clone().users();
-        let user = users.first().expect("Failed to get user");
-        assert_that!(&user.username, is(equal_to("jeff")));
-        Ok(())
-    }
-
-    #[test]
-    fn returns_main_github() -> Result<()> {
+    fn returns_default_github() -> Result<()> {
         let fakehub = Fakehub::default();
         let github = fakehub.main();
         let users = github.clone().users();
-        assert_that!(github.clone().id.is_nil(), is(equal_to(false)));
+        let user = users.first().expect("Failed to get user");
         assert_that!(&github.name, is(equal_to("main")));
-        assert_that!(users.len(), is(equal_to(1)));
+        assert_that!(&user.username, is(equal_to("jeff")));
         Ok(())
     }
 }
