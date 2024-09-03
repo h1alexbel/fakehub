3c924f2ebf37af88dd0ab957559420e76c2e8759
h1alexbel <h1alexbelx@gmail.com>
2024-09-03 15:16:25 +0300

    feat(#137): user extra

--- server/src/objects/user.rs
+++ server/src/objects/user.rs
@@ -19,19 +19,25 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+use crate::handlers::cursor::Cursor;
+use crate::handlers::sh_cursor::ShCursor;
 use crate::objects::github::GitHub;
 use crate::objects::repo::Repo;
 use anyhow::Result;
 use log::info;
+use rand::Rng;
 use serde::{Deserialize, Serialize};
+use serde_json::{Map, Number, Value};
 
 /// GitHub user.
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct User {
-    /// Username.
-    pub username: String,
+    /// Login, a.k.a. username.
+    pub login: String,
     /// Repos.
     pub repos: Vec<Repo>,
+    /// Extra information.
+    pub extra: Map<String, Value>,
 }
 
 impl User {
@@ -49,8 +55,9 @@ impl User {
     /// ```
     pub fn new(username: String) -> User {
         User {
-            username,
+            login: username,
             repos: vec![],
+            extra: Map::new(),
         }
     }
 
@@ -60,16 +67,175 @@ impl User {
     ///```
     /// use server::objects::fakehub::FakeHub;
     /// use server::objects::user::User;
+    ///
     /// let fakehub = FakeHub::default();
     /// let mut github = fakehub.main().clone();
     /// User::new(String::from("foo")).register_in(&mut github).expect("Failed to register user");
     ///```
-    pub fn register_in(&self, github: &mut GitHub) -> Result<(), String> {
-        match github.user(&self.username) {
-            Some(u) => Err(format!("User with login @{} already exists!", u.username)),
+    // @todo #137:30min Create NodeId object that will give MD5 hash.
+    //  Almost in any response, including user, we should display Node ID as
+    //  GitHub API does. Looks like they use MD5 for it. Let's do the same, but
+    //  we need only one that hash, because only one instance can be run. Let's
+    //  hash UTC datetime when server was bootstrapped.
+    // @todo #137:35min Pass port to the base cursor. Now we hardcode the
+    //  address localhost:3000, however we need to handle generic ports that
+    //  users can set.
+    pub fn register_in(&mut self, github: &mut GitHub) -> Result<(), String> {
+        match github.user(&self.login) {
+            Some(u) => Err(format!("User with login @{} already exists!", u.login)),
             None => {
+                let cursor = Cursor {
+                    base: String::from("localhost:3000"),
+                };
+                let id = rand::thread_rng().gen_range(0..100_000_000);
+                self.extra
+                    .insert(String::from("node_id"), Value::String(String::from("")));
+                self.extra
+                    .insert(String::from("id"), Value::Number(Number::from(id)));
+                self.extra.insert(
+                    String::from("avatar_url"),
+                    Value::String(format!("u/{}?v=4", id)),
+                );
+                self.extra
+                    .insert(String::from("gravatar_id"), Value::String(String::from("")));
+                self.extra.insert(
+                    String::from("url"),
+                    Value::String(
+                        ShCursor::new(cursor.clone(), format!("users/{}", self.login))
+                            .as_string(),
+                    ),
+                );
+                self.extra.insert(
+                    String::from("html_url"),
+                    Value::String(
+                        ShCursor::new(cursor.clone(), self.login.to_string()).as_string(),
+                    ),
+                );
+                self.extra.insert(
+                    String::from("followers_url"),
+                    Value::String(
+                        ShCursor::new(
+                            cursor.clone(),
+                            format!("users/{}/followers", self.login),
+                        )
+                        .as_string(),
+                    ),
+                );
+                let following = ShCursor::new(
+                    cursor.clone(),
+                    format!("users/{}/following", self.login),
+                )
+                .as_string();
+                self.extra.insert(
+                    String::from("following_url"),
+                    Value::String(format!(
+                        "[{}{{/other_user}}]({}%7B/other_user%7D)",
+                        following, following
+                    )),
+                );
+                let gists =
+                    ShCursor::new(cursor.clone(), format!("users/{}/gists", self.login))
+                        .as_string();
+                self.extra.insert(
+                    String::from("gists_url"),
+                    Value::String(format!(
+                        "[{}{{/gist_id}}]({}%7B/gist_id%7D)",
+                        gists, gists
+                    )),
+                );
+                let starred = ShCursor::new(
+                    cursor.clone(),
+                    format!("users/{}/starred", self.login),
+                )
+                .as_string();
+                self.extra.insert(
+                    String::from("starred_url"),
+                    Value::String(format!(
+                        "[{}{{/owner}}{{/repo}}]({}%7B/owner%7D%7B/repo%7D)",
+                        starred, starred
+                    )),
+                );
+                self.extra.insert(
+                    String::from("subscriptions_url"),
+                    Value::String(
+                        ShCursor::new(
+                            cursor.clone(),
+                            format!("users/{}/subscriptions", self.login),
+                        )
+                        .as_string(),
+                    ),
+                );
+                self.extra.insert(
+                    String::from("organizations_url"),
+                    Value::String(
+                        ShCursor::new(
+                            cursor.clone(),
+                            format!("users/{}/orgs", self.login),
+                        )
+                        .as_string(),
+                    ),
+                );
+                self.extra.insert(
+                    String::from("repos_url"),
+                    Value::String(
+                        ShCursor::new(
+                            cursor.clone(),
+                            format!("users/{}/repos", self.login),
+                        )
+                        .as_string(),
+                    ),
+                );
+                let events =
+                    ShCursor::new(cursor.clone(), format!("users/{}/events", self.login))
+                        .as_string();
+                self.extra.insert(
+                    String::from("events_url"),
+                    Value::String(format!(
+                        "[{}{{/privacy}}]({}%7B/privacy%7D)",
+                        events, events
+                    )),
+                );
+                self.extra.insert(
+                    String::from("received_events_url"),
+                    Value::String(
+                        ShCursor::new(
+                            cursor,
+                            format!("users/{}/received_events", self.login),
+                        )
+                        .as_string(),
+                    ),
+                );
+                self.extra
+                    .insert(String::from("type"), Value::String(String::from("User")));
+                self.extra
+                    .insert(String::from("site_admin"), Value::Bool(false));
+                self.extra.insert(
+                    String::from("name"),
+                    Value::String(String::from("FakeHub user")),
+                );
+                self.extra.insert(String::from("company"), Value::Null);
+                self.extra.insert(String::from("blog"), Value::Null);
+                self.extra.insert(String::from("location"), Value::Null);
+                self.extra.insert(String::from("email"), Value::Null);
+                self.extra.insert(String::from("hireable"), Value::Null);
+                self.extra.insert(String::from("bio"), Value::Null);
+                self.extra
+                    .insert(String::from("twitter_username"), Value::Null);
+                self.extra
+                    .insert(String::from("public_repos"), Value::Number(Number::from(0)));
+                self.extra
+                    .insert(String::from("public_gists"), Value::Number(Number::from(0)));
+                self.extra
+                    .insert(String::from("followers"), Value::Number(Number::from(0)));
+                self.extra
+                    .insert(String::from("following"), Value::Number(Number::from(0)));
+                let now = chrono::offset::Utc::now();
+                self.extra
+                    .insert(String::from("created_at"), Value::String(now.to_string()));
+                self.extra
+                    .insert(String::from("updated_at"), Value::String(now.to_string()));
                 github.add_user(self.clone());
-                info!("Registered @{}", self.username);
+                info!("Registered @{}", self.login);
                 Ok(())
             }
         }
@@ -87,7 +253,7 @@ mod tests {
     fn returns_username() -> Result<()> {
         let expected = "jeff";
         let jeff = User::new(String::from(expected));
-        assert_that!(jeff.username, is(equal_to(String::from(expected))));
+        assert_that!(jeff.login, is(equal_to(String::from(expected))));
         Ok(())
     }
 
@@ -100,7 +266,7 @@ mod tests {
             .register_in(&mut github)
             .expect("Failed to register user");
         let pulled = github.users.get(&foo).expect("Failed to get user");
-        assert_that!(pulled.clone().username, is(equal_to(foo)));
+        assert_that!(pulled.clone().login, is(equal_to(foo)));
         Ok(())
     }
 
@@ -112,5 +278,19 @@ mod tests {
         User::new(String::from("jeff"))
             .register_in(&mut github)
             .expect("Failed to register user");
+    }
+
+    #[test]
+    fn registers_with_extra() -> Result<()> {
+        let fakehub = FakeHub::default();
+        let mut github = fakehub.main();
+        User::new(String::from("foo"))
+            .register_in(&mut github)
+            .expect("Failed to register user");
+        let user = github.users.get("foo").expect("Failed to get user");
+        let url = user.extra.get("url").expect("Failed to read property");
+        assert_that!(url.as_str(), is(equal_to(Some("localhost:3000/users/foo"))));
+        assert_that!(user.extra.len(), is(equal_to(31)));
+        Ok(())
     }
 }
