c6c7ee5f624a7f3943c18eef40a01db7ce0ee694
h1alexbel <h1alexbelx@gmail.com>
2024-09-10 20:15:59 +0300

    feat(#147): sort_by_key

--- server/src/handlers/users.rs
+++ server/src/handlers/users.rs
@@ -1,3 +1,24 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2024 Aliaksei Bialiauski
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 use crate::objects::json::json_user::JsonUser;
 use crate::ServerConfig;
 use axum::extract::State;
@@ -9,13 +30,13 @@ use serde_json::Value;
 pub async fn users(State(config): State<ServerConfig>) -> Json<Vec<Value>> {
     let fakehub = config.fakehub;
     let github = fakehub.main();
-    Json(
-        github
-            .users()
-            .iter()
-            .map(|u| JsonUser::new(u.clone()).as_json())
-            .collect(),
-    )
+    let mut users: Vec<Value> = github
+        .users()
+        .iter()
+        .map(|u| JsonUser::new(u.clone()).as_json())
+        .collect();
+    users.sort_by_key(|user| user.get("id").and_then(|id| id.as_u64()));
+    Json(users)
 }
 
 #[cfg(test)]
