91f2e3b029b59f4a9ceb99969989aba4e93ed904
h1alexbel <h1alexbelx@gmail.com>
2024-10-08 19:45:31 +0300

    feat(#77): doc typo

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 /*!
-Fakehub server and storage.
+Fakehub cli.
  */
 use clap::Parser;
 use log::{error, info};
