7a2984a567308759a2488fbfaf417bd5958d145f
h1alexbel <h1alexbelx@gmail.com>
2024-10-09 18:46:48 +0300

    feat(#196): just gen

--- tests/full-cycle.sh
+++ tests/full-cycle.sh
@@ -19,6 +19,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+just gen
 {
   cargo run -- start -p 3000 -d
   out="$(curl -s -o /dev/null -I -w "%{http_code}" localhost:3000)"
