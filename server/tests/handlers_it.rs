aca9425221042074234ee42736912ce47bb22358
h1alexbel <h1alexbelx@gmail.com>
2025-02-03 13:00:51 +0300

    chore: missing-docs

--- server/tests/handlers_it.rs
+++ server/tests/handlers_it.rs
@@ -19,6 +19,9 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+/*!
+Integration tests for handlers.
+ */
 #[allow(clippy::question_mark_used)]
 #[cfg(test)]
 mod routes_its {
