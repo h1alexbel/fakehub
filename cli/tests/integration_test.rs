bbb8689a14e937bf50f6adb4a11754e1154b51ae
h1alexbel <h1alexbelx@gmail.com>
2025-02-03 13:22:00 +0300

    chore: fixed all missing-docs

--- cli/tests/integration_test.rs
+++ cli/tests/integration_test.rs
@@ -19,6 +19,9 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+/*!
+CLI integration tests.
+ */
 #[allow(clippy::question_mark_used)]
 #[cfg(test)]
 mod tests {
